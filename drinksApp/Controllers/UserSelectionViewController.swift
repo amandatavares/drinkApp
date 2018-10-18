cafae4a612d87eac97a8d05983b73724244b6846
amandatavares <amandatvt@gmail.com>
2018-10-18 10:26:45 -0300

    Add new controllers and views

---
+++ drinksApp/Controllers/UserSelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserSelectionViewController.swift
+//  drinksApp
+//
+//  Created by Amanda Tavares on 18/10/18.
+//  Copyright © 2018 Amanda Tavares. All rights reserved.
+//
+
+import UIKit
+
+class UserSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
